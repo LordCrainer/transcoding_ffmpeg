eca4f7b79d549d9d7442dde6be00268803497dd6
LordCrainer <camogan3000@hotmail.com>
2020-12-09 14:15:14 -0500

    Reestructurando a clean arquitecture

--- api/src/2.application/routes/v1/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const transcoding = require("../../components/transcoding/1.infrastructure/router");
-
-router.use("transcoding", transcoding(router));
-
-module.exports = router;
--- server/src/api/components/transcoding/3.Domain/commads.js
+++ server/src/api/components/transcoding/3.Domain/commads.js
@@ -16,7 +16,7 @@ const dv25Mxf = ({ origin, destiny }) =>
 const preAjust = ({ origin, destiny }) =>
   `ffmpeg -i ${origin} -r 29970/1000 -vcodec mpeg4 -pix_fmt yuv420p  -vf eq=saturation=1.06,scale=640:480:force_original_aspect_ratio=decrease,pad=640:480:(ow-iw)/2:(oh-ih)/2,setsar=1 -q:v 1 -b:v 50M -maxrate 50M  -minrate 50M -bufsize 8M  -acodec pcm_s16le  -timecode 00:00:00:00 -y ${destiny}`;
 
-const preAjustSD = ({}) =>
+const preAjustSD = ({ origin, destiny }) =>
   `ffmbc -i ${origin} -r 29970/1000 -vcodec mpeg4  -pix_fmt yuv420p -vf pad=720:576:0:72:black:aspect=4:3  -qscale 1 -color_primaries bt709 -b 50M -maxrate 50M  -minrate 50M -bufsize 8M  -acodec pcm_s16le  -timecode 00:00:00:00 -y ${destiny}`;
 
 module.exports = {
@@ -24,4 +24,5 @@ module.exports = {
   editVolume,
   dv25Mov,
   dv25Mxf,
+  preAjustSD,
 };
