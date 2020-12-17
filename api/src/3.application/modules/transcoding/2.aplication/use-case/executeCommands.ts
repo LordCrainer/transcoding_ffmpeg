7efd9dbfdf96555c74e58992c2a2278a250a9aa8
LordCrainer <camogan3000@hotmail.com>
2020-12-17 11:38:17 -0500

    Proceso de ejecución de los comandos

--- api/src/3.application/modules/transcoding/2.aplication/use-case/executeCommands.ts
+++ api/src/3.application/modules/transcoding/2.aplication/use-case/executeCommands.ts
@@ -1,5 +1,22 @@
 import { executeProcess } from "../service";
 
+/* const getVolumen = async ({ origin }) => {
+  let stderr, max, mean;
+  try {
+    const [program, ...args] = utilsTranscoder.splitString(
+      commands.volumeDetect({ origin })
+    );
+    ({ stderr } = await asyncSpawnExec({
+      program,
+      args,
+    }));
+    ({ max, mean } = handledData.getMaxAndMean(stderr, regexs.volume));
+  } catch (err) {
+    throw err;
+  }
+  return { max, mean };
+}; */
+
 const executeCommands = () => {
   // split el comando necesario
   // ejecuto el comando, extrayendo las variables y ejecuto la función de salida
